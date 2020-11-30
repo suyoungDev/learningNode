356c846947cf7f7de939a10bf1e0feb82ea878ff
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-30 18:39:18 +0900

    추가: 계산식

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send(`here's the calc: ${num1} + ${num2} = ${result}`)
 })
 
 app.listen(port, () => {
