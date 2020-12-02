390c2628ad40726c4eec77dbb2abbf6c890639b0
Mocha <71932072+mochapoke@users.noreply.github.com>
2020-12-02 12:41:32 +0900

    추가: https.get 으로 JSON 데이터 가져오기
    native 모듈이라 따로 설치안해도 괜찮음!

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -6,10 +6,17 @@ const app = express();
 app.get('/', function(req, res){
   const url = 'http://api.openweathermap.org/data/2.5/weather?q=Incheon&appid=02e441a38cd4dea2f66af42e94f8dc29&units=metric';
 
-  https.get(url, function(response){
-    console.log(response);
+  https.get(url, (response)=>{
+    console.log( 'https에러코드: ' + response.statusCode); // http 에러코드 
+
+    response.on('data', (data)=>{
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      console.log(temp);
+      console.log(weatherDescription);
+    })
   })
-})
 
 app.listen(3000, function(){
   console.log('server is running on port 3000');
